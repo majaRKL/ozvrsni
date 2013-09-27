5e65dccd59bcb266f9ce3db7a8991fab8ef1dd87
Maja Soldo <maja@oz.(none)>
2013-09-27 21:44:55 +0200

    dodani znakovi, pretvoreni framovu i greyscale, pozvan matchTamplate

--- video-template-matching.cpp
+++ video-template-matching.cpp
@@ -8,22 +8,48 @@ using namespace cv;
 
 int main( int argc, char** argv )
 {
-    VideoCapture cap("znakich2.mp4");
+    VideoCapture cap("video/znakich2.mp4");
 	if(!cap.isOpened())  // check if we succeeded
         return -1;
 
 	Rect rect;
-    Mat roi;
-    //namedWindow("frame",1);
-    namedWindow("roi",1);
+    Mat frame, roi, groi, results;
+	Mat znak1, znak2, znak3, znak4;
+	znak1 = imread ("roi/01_roi.png");		
+	znak2 = imread ("roi/02_roi.png");
+	znak3 = imread ("roi/03_roi.png");
+	znak4 = imread ("roi/04_roi.png");
+	cvtColor(znak1, znak1, CV_BGR2GRAY);
+	
+	cap >> frame;
+	rect = Rect (600, 150, 480, 120);
+    roi = frame(rect);
+	
+	int resultRows, resultCols;
+	resultRows = roi.rows - znak1.rows + 1;
+	resultCols = roi.cols - znak1.cols + 1;
+	results.create( resultRows, resultCols, CV_32FC1);
+	
+	//namedWindow("frame",1);
+	
+	double minVal; double maxVal; 
+	Point minLoc; Point maxLoc; Point matchLoc;
+    
     for(;;)
-    {
-        Mat frame;
-        cap >> frame; // get a new frame from camera              
+    {        
+        cap >> frame;              
         //imshow("frame", frame);
-        rect = Rect (600, 150, 480, 120);
         roi = frame(rect);
-        imshow("roi", roi);
+        cvtColor(roi, groi, CV_BGR2GRAY);
+        
+        matchTemplate(groi, znak1, results, 5);
+        normalize(results, results, 0, 1, NORM_MINMAX, -1);
+        threshold(results, results, 0.8, 1, THRESH_BINARY);
+		minMaxLoc(results, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+		rectangle( groi, maxLoc, Point( maxLoc.x + znak1.cols , maxLoc.y + znak1.rows ), Scalar::all(0), 2, 8, 0 );
+        imshow("groi", groi);
+        imshow("results", results);
+        
         if(waitKey(30) >= 0) break;
     }
     return 0;
